04a0dd4ee64d679ea91fe6f1e2d79db6ec84fe1d
skjoshi13 <skjoshi13@2210af32-f2a1-504b-2c71-20ec49948f07>
2014-02-14 08:54:15 +0000

    Updated

--- src/com/ba/action/RegisterAction.java
+++ src/com/ba/action/RegisterAction.java
@@ -1,10 +1,10 @@
 package com.ba.action;
 
+import java.util.Map;
+
 import com.ba.constants.Constants;
-import com.ba.dao.impl.RegisterDAO;
-import com.ba.exception.BAException;
-import com.ba.factory.GenericFactory;
 import com.ba.pojo.RegisterBean;
+import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class RegisterAction extends ActionSupport{
@@ -12,15 +12,17 @@ public class RegisterAction extends ActionSupport{
 	private static final long serialVersionUID = 1L;
 	private RegisterBean rb =new RegisterBean();
 	
+
+	
 	public String execute() {
-		try{
-			
+		/*try{
 			RegisterDAO dao = (RegisterDAO)GenericFactory.buildObject(Constants.REGISTER_DAO);
 			dao.registerUser(rb);
-			
 		}catch(BAException e){
 			System.out.println(e);
-		}
+		}*/
+		Map<String, Object> session = ActionContext.getContext().getSession();
+		session.put("username", rb.getEmail());	
 		return "success";
 	}
 
@@ -31,6 +33,8 @@ public class RegisterAction extends ActionSupport{
 	public void setRb(RegisterBean rb) {
 		this.rb = rb;
 	}
+
+
 	
 
 }
