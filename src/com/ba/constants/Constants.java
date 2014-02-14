b345c74ed2da38377c61fdb82052b72a65c8d685
skjoshi13 <skjoshi13@2210af32-f2a1-504b-2c71-20ec49948f07>
2014-02-14 08:54:27 +0000

    Updated

--- src/com/ba/constants/Constants.java
+++ src/com/ba/constants/Constants.java
@@ -1,9 +1,20 @@
 package com.ba.constants;
 
 public class Constants {
-
+	
+	
+	//Global String Constants
 	public static String SUCCESS = "success";
+	
+	//Context params
 	public static String DATASOURCE ="java:comp/env/jdbc/BookAddaDS";
 	public static String REGISTER_DAO ="java:comp/env/param/RegisterDAO";
+	public static String LOGIN_DAO ="java:comp/env/param/LoginDAO";
+	
+	//Stored Procedures, Functions and SQLs
+	public static String REGISTER_USER_PROC = "call p_register_user(?,?,?,?)";
+	public static String LOGIN_USER = "select email, first_name, last_name from userlogin where username=? and password=?";
+	
+
 	
 }
