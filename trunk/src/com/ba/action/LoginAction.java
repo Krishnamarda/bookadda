1950bdcfb9954ced5706d96ca7ad34a54e2839a6
skjoshi13 <skjoshi13@2210af32-f2a1-504b-2c71-20ec49948f07>
2014-02-14 08:54:01 +0000

    Updated

--- trunk/src/com/ba/action/LoginAction.java
+++ trunk/src/com/ba/action/LoginAction.java
@@ -1,27 +1,32 @@
 package com.ba.action;
 
+import java.util.Map;
+
 import com.ba.constants.Constants;
+import com.ba.dao.impl.LoginDAO;
+import com.ba.exception.BAException;
+import com.ba.factory.GenericFactory;
+import com.opensymphony.xwork2.ActionContext;
 
 public class LoginAction {
-	
+
 	private String username;
 	private String password;
-	
+
 	/**
 	 * @return the username
 	 */
 	public String getUsername() {
 		return username;
 	}
+
 	/**
-	 * @param username the username to set
+	 * @param username
+	 *            the username to set
 	 */
 	public void setUsername(String username) {
 		this.username = username;
 	}
-
-
-
 
 	/**
 	 * @return the password
@@ -29,18 +34,35 @@ public class LoginAction {
 	public String getPassword() {
 		return password;
 	}
+
 	/**
-	 * @param password the password to set
+	 * @param password
+	 *            the password to set
 	 */
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
-	
-	
-	public String doLogin() throws Exception {		 
-		System.out.println("Inside Login"+getUsername()+getPassword());
+
+	public String execute() throws Exception {
+		/*try {
+			LoginDAO dao = (LoginDAO) GenericFactory
+					.buildObject(Constants.LOGIN_DAO);
+			dao.userLogin(username, password);
+		} catch (BAException e) {
+			System.out.println(e);
+		}*/
+		if(getUsername() == null){
+			return "logout";
+		}
+		Map<String, Object> session = ActionContext.getContext().getSession();
+		if(session.get("username")!=null){
+			return Constants.SUCCESS;
+		}else{
+			session.put("username", getUsername());	
+		}
+		
 		return Constants.SUCCESS;
+		
 	}
 
 }
