7870f6dfa78cee78997ce4140733dc8f65b27dcc
skjoshi13 <skjoshi13@2210af32-f2a1-504b-2c71-20ec49948f07>
2014-02-18 13:24:08 +0000

    Adding to the code repository

---
+++ trunk/src/com/ba/pojo/Ownlist.java
@@ -0,0 +1,43 @@
+package com.ba.pojo;
+
+public class Ownlist {
+	
+	private String ownlist_id;
+	private String book_name;
+	private String author;
+	private String availtype;
+	private int quantity;
+	public String getOwnlist_id() {
+		return ownlist_id;
+	}
+	public void setOwnlist_id(String ownlist_id) {
+		this.ownlist_id = ownlist_id;
+	}
+	public String getBook_name() {
+		return book_name;
+	}
+	public void setBook_name(String book_name) {
+		this.book_name = book_name;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getAvailtype() {
+		return availtype;
+	}
+	public void setAvailtype(String availtype) {
+		this.availtype = availtype;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	
+	
+
+}
---
+++ trunk/src/com/ba/pojo/Wishlist.java
@@ -0,0 +1,43 @@
+package com.ba.pojo;
+
+public class Wishlist {
+
+	private String wishlist_id;
+	private String book_name;
+	private String author;
+	private String wishtype;
+	private int quantity;
+	public String getWishlist_id() {
+		return wishlist_id;
+	}
+	public void setWishlist_id(String wishlist_id) {
+		this.wishlist_id = wishlist_id;
+	}
+	public String getBook_name() {
+		return book_name;
+	}
+	public void setBook_name(String book_name) {
+		this.book_name = book_name;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getWishtype() {
+		return wishtype;
+	}
+	public void setWishtype(String wishtype) {
+		this.wishtype = wishtype;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	
+	
+
+}
