f96316332c3705908cf1b967a06e316143a18a4a
skjoshi13 <skjoshi13@2210af32-f2a1-504b-2c71-20ec49948f07>
2014-02-18 13:25:47 +0000

    Updated

--- src/com/ba/constants/Constants.java
+++ src/com/ba/constants/Constants.java
@@ -5,6 +5,14 @@ public class Constants {
 	
 	//Global String Constants
 	public static String SUCCESS = "success";
+	public static String LOGIN_ERROR = "loginError";
+	public static String LOGIN = "login";
+	public static String REGISTER = "register";
+	public static String TRUE = "true";
+	public static String FALSE = "false";
+	public static String USERNAME = "username";
+	public static String DUPLICATE = "duplicate";
+	public static String DUPLICATE_USER_REGISTRATION = "duplciateUser";
 	
 	//Context params
 	public static String DATASOURCE ="java:comp/env/jdbc/BookAddaDS";
@@ -12,8 +20,12 @@ public class Constants {
 	public static String LOGIN_DAO ="java:comp/env/param/LoginDAO";
 	
 	//Stored Procedures, Functions and SQLs
-	public static String REGISTER_USER_PROC = "call p_register_user(?,?,?,?)";
-	public static String LOGIN_USER = "select email, first_name, last_name from userlogin where username=? and password=?";
+	public static String REGISTER_USER = "insert into user_info values (?,?,?,?,?,?,?,?,?,?)";
+	public static String DUPLICATE_USER_CHECK = "select 1 from user_info where UPPER(email) = UPPER(?)";
+	public static String LOGIN_USER_1 = "select email, fname, lname, gender, wishlist_id, ownlist_id, inst_id from user_info where email=? and password=?";
+	public static String LOGIN_USER_2 = "select email, fname, lname, gender, wishlist_id, ownlist_id, inst_id from user_info where email=?";
+	public static String GET_WISH_LIST = "select * from wishlist where wishlist_id=?";
+	public static String GET_OWN_LIST = "select * from ownlist where ownlist_id=?";
 	
 
 	
