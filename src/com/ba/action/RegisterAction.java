6c8b2cc309c8dab7ac5e576941751b3c1442de3f
skjoshi13 <skjoshi13@2210af32-f2a1-504b-2c71-20ec49948f07>
2014-02-18 13:26:32 +0000

    Updated

--- src/com/ba/action/RegisterAction.java
+++ src/com/ba/action/RegisterAction.java
@@ -3,6 +3,9 @@ package com.ba.action;
 import java.util.Map;
 
 import com.ba.constants.Constants;
+import com.ba.dao.impl.RegisterDAO;
+import com.ba.exception.BAException;
+import com.ba.factory.GenericFactory;
 import com.ba.pojo.RegisterBean;
 import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
@@ -11,19 +14,20 @@ public class RegisterAction extends ActionSupport{
 	
 	private static final long serialVersionUID = 1L;
 	private RegisterBean rb =new RegisterBean();
-	
-
-	
+	private String isUserDuplicate;
 	public String execute() {
-		/*try{
+		try{
 			RegisterDAO dao = (RegisterDAO)GenericFactory.buildObject(Constants.REGISTER_DAO);
-			dao.registerUser(rb);
+			isUserDuplicate = dao.registerUser(rb);
+			if(Constants.TRUE.equals(isUserDuplicate)){
+				return Constants.DUPLICATE_USER_REGISTRATION;
+			}
+			Map<String, Object> session = ActionContext.getContext().getSession();
+			session.put(Constants.USERNAME, rb.getEmail());	
 		}catch(BAException e){
 			System.out.println(e);
-		}*/
-		Map<String, Object> session = ActionContext.getContext().getSession();
-		session.put("username", rb.getEmail());	
-		return "success";
+		}
+		return Constants.SUCCESS;
 	}
 
 	public RegisterBean getRb() {
@@ -32,6 +36,10 @@ public class RegisterAction extends ActionSupport{
 
 	public void setRb(RegisterBean rb) {
 		this.rb = rb;
+	}
+
+	public String getIsUserDuplicate() {
+		return isUserDuplicate;
 	}
 
 
