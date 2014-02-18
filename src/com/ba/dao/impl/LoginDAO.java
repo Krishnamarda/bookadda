9efda0131e7744565ac4dd027b6f4b591cadace8
skjoshi13 <skjoshi13@2210af32-f2a1-504b-2c71-20ec49948f07>
2014-02-18 13:25:26 +0000

    Updated

--- src/com/ba/dao/impl/LoginDAO.java
+++ src/com/ba/dao/impl/LoginDAO.java
@@ -3,35 +3,108 @@ package com.ba.dao.impl;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.ba.constants.Constants;
 import com.ba.exception.BAException;
+import com.ba.pojo.Ownlist;
 import com.ba.pojo.UserDetails;
+import com.ba.pojo.Wishlist;
 import com.ba.utils.ConnectionFactory;
 
 public class LoginDAO extends ConnectionFactory {
-	
 
-	public UserDetails userLogin(String unm, String pwd) throws BAException{
-		Connection con = null;
-		PreparedStatement cStmt = null;
+	private Connection con = null;
+	private PreparedStatement pStmt = null;
+
+	public UserDetails userLogin(String unm, String pwd, String loginType) throws BAException {
+
 		int colCount = 1;
 		ResultSet rSet = null;
-		UserDetails ud = UserDetails.getInstance();
+		UserDetails ud = null;
 		try {
 			con = getConnection(Constants.DATASOURCE);
-			cStmt = con.prepareStatement(Constants.LOGIN_USER);
-			rSet = cStmt.executeQuery();
-			rSet.next();
-			ud.setEmail(rSet.getString(colCount++));
-			ud.setFname(rSet.getString(colCount++));
-			ud.setLname(rSet.getString(colCount++));
-		} catch (Exception sqle) {
-			throw new BAException(sqle, "Exception caught in UserDetails.userLogin()");
+			if(Constants.LOGIN.equals(loginType)){
+				pStmt = con.prepareStatement(Constants.LOGIN_USER_1);
+				pStmt.setString(1, unm);
+				pStmt.setString(2, pwd);
+			}else{
+				pStmt = con.prepareStatement(Constants.LOGIN_USER_2);
+				pStmt.setString(1, unm);
+			}
+			
+			rSet = pStmt.executeQuery();
+			if (rSet.next()) {
+				ud = UserDetails.getInstance();
+				ud.setEmail(rSet.getString(colCount++));
+				ud.setFname(rSet.getString(colCount++));
+				ud.setLname(rSet.getString(colCount++));
+				ud.setGender(rSet.getString(colCount++));
+				ud.setWishlist_id(rSet.getString(colCount++));
+				ud.setOwnlist_id(rSet.getString(colCount++));
+				ud.setInst_id(rSet.getString(colCount++));
+				ud.setWishList(getWishList(ud.getWishlist_id()));
+				ud.setOwnList(getOwnList(ud.getOwnlist_id()));
+			}
+
+		} catch (SQLException sqle) {
+			throw new BAException(sqle,
+					"Exception caught in UserDetails.userLogin()");
 		}
 
 		return ud;
 
 	}
 
+	private List<Wishlist> getWishList(String wishlist_id) throws BAException {
+		List<Wishlist> wishlist = new ArrayList<Wishlist>();
+		try {
+			pStmt = con.prepareStatement(Constants.GET_WISH_LIST);
+			pStmt.setString(1, wishlist_id);
+			ResultSet rSet = null;
+			rSet = pStmt.executeQuery();
+			int colCount = 1;
+
+			while (rSet.next()) {
+				Wishlist wl = new Wishlist();
+				wl.setWishlist_id(rSet.getString(colCount));
+				wl.setBook_name(rSet.getString(colCount));
+				wl.setAuthor(rSet.getString(colCount));
+				wl.setWishtype(rSet.getString(colCount));
+				wl.setQuantity(rSet.getInt(colCount));
+				wishlist.add(wl);
+			}
+		} catch (SQLException sqle) {
+			throw new BAException(sqle,
+					"Exception caught in LoginDAO.getWishList()");
+		}
+		return wishlist;
+	}
+
+	private List<Ownlist> getOwnList(String ownlist_id) throws BAException {
+		List<Ownlist> ownlist = new ArrayList<Ownlist>();
+		try {
+			pStmt = con.prepareStatement(Constants.GET_WISH_LIST);
+			pStmt.setString(1, ownlist_id);
+			ResultSet rSet = null;
+			rSet = pStmt.executeQuery();
+			int colCount = 1;
+
+			while (rSet.next()) {
+				Ownlist ol = new Ownlist();
+				ol.setOwnlist_id(rSet.getString(colCount));
+				ol.setBook_name(rSet.getString(colCount));
+				ol.setAuthor(rSet.getString(colCount));
+				ol.setAvailtype(rSet.getString(colCount));
+				ol.setQuantity(rSet.getInt(colCount));
+				ownlist.add(ol);
+			}
+		} catch (SQLException sqle) {
+			throw new BAException(sqle,
+					"Exception caught in LoginDAO.getOwnList()");
+		}
+		return ownlist;
+	}
 }
--- src/com/ba/dao/impl/RegisterDAO.java
+++ src/com/ba/dao/impl/RegisterDAO.java
@@ -1,9 +1,10 @@
 package com.ba.dao.impl;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Calendar;
 
 import com.ba.constants.Constants;
 import com.ba.exception.BAException;
@@ -13,28 +14,36 @@ import com.ba.utils.ConnectionFactory;
 
 public class RegisterDAO extends ConnectionFactory {
 	
-	public UserDetails registerUser(RegisterBean rb) throws BAException {
+	public String registerUser(RegisterBean rb) throws BAException {
 		Connection con = null;
-		PreparedStatement cStmt = null;
+		PreparedStatement pStmt = null;
 		int colCount=1;
-		ResultSet rSet = null;
 		UserDetails ud = UserDetails.getInstance();
+		String isUserDuplicate = Constants.FALSE;
 		try {
 			con = getConnection(Constants.DATASOURCE);
-			cStmt = con.prepareCall(Constants.REGISTER_USER_PROC);
-			cStmt.setString(colCount++, rb.getFname());
-			cStmt.setString(colCount++, rb.getLname());
-			cStmt.setString(colCount++, rb.getEmail());
-			cStmt.setString(colCount++, rb.getPassword());
-			rSet = cStmt.executeQuery();
-			 while(rSet.next()){
-				 
-			 }
-
+			pStmt = con.prepareStatement(Constants.DUPLICATE_USER_CHECK);
+			pStmt.setString(1, rb.getEmail());
+			if(pStmt.executeQuery().next()){
+				isUserDuplicate=Constants.TRUE;
+			}else{
+				pStmt = con.prepareStatement(Constants.REGISTER_USER);
+				pStmt.setString(colCount++, rb.getEmail());
+				pStmt.setString(colCount++, rb.getFname());
+				pStmt.setString(colCount++, rb.getLname());
+				pStmt.setString(colCount++, rb.getPassword());
+				pStmt.setString(colCount++, "");
+				pStmt.setString(colCount++, "");
+				pStmt.setString(colCount++, "");
+				pStmt.setInt(colCount++,0);
+				pStmt.setDate(colCount++,new java.sql.Date(new java.util.Date().getTime()));
+				pStmt.setDate(colCount++,new java.sql.Date(new java.util.Date().getTime()));
+				pStmt.executeUpdate();
+			}
+			return isUserDuplicate;
 		} catch (SQLException sqle) {
 			throw new BAException(sqle, "Exception caught in RegisterDAO.registerUser()");
 		}
-	    return ud;
 
 	}
 
