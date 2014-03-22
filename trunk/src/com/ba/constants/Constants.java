ee4dee21ad4bdb0c2f14d5b52073aa5fe4812288
skjoshi13 <skjoshi13@2210af32-f2a1-504b-2c71-20ec49948f07>
2014-03-22 10:51:03 +0000

    Updated

--- trunk/src/com/ba/constants/Constants.java
+++ trunk/src/com/ba/constants/Constants.java
@@ -1,9 +1,8 @@
 package com.ba.constants;
 
 public class Constants {
-	
-	
-	//Global String Constants
+
+	// Global String Constants
 	public static String SUCCESS = "success";
 	public static String LOGIN_ERROR = "loginError";
 	public static String LOGIN = "login";
@@ -13,20 +12,26 @@ public class Constants {
 	public static String USERNAME = "username";
 	public static String DUPLICATE = "duplicate";
 	public static String DUPLICATE_USER_REGISTRATION = "duplciateUser";
-	
-	//Context params
-	public static String DATASOURCE ="java:comp/env/jdbc/BookAddaDS";
-	public static String REGISTER_DAO ="java:comp/env/param/RegisterDAO";
-	public static String LOGIN_DAO ="java:comp/env/param/LoginDAO";
-	
-	//Stored Procedures, Functions and SQLs
+
+	// Context params
+	public static String DATASOURCE = "java:comp/env/jdbc/BookAddaDS";
+	public static String REGISTER_DAO = "java:comp/env/param/RegisterDAO";
+	public static String LOGIN_DAO = "java:comp/env/param/LoginDAO";
+	public static String HOME_DAO = "java:comp/env/param/HomeDAO";
+	public static String ADD_OWN_DAO = "java:comp/env/param/AddOwnDAO";
+	public static String ADD_WISH_DAO = "java:comp/env/param/AddWishDAO";
+
+	// Stored Procedures, Functions and SQLs
 	public static String REGISTER_USER = "insert into user_info values (?,?,?,?,?,?,?,?,?,?)";
 	public static String DUPLICATE_USER_CHECK = "select 1 from user_info where UPPER(email) = UPPER(?)";
 	public static String LOGIN_USER_1 = "select email, fname, lname, gender, wishlist_id, ownlist_id, inst_id from user_info where email=? and password=?";
 	public static String LOGIN_USER_2 = "select email, fname, lname, gender, wishlist_id, ownlist_id, inst_id from user_info where email=?";
+	public static String GET_HOME_DATA = "select * from ownlist where ownlist_id in" +
+			"(select email from user_info where inst_id = " +
+			"(select inst_id from user_info  where email=?) and email <> ?) ";
 	public static String GET_WISH_LIST = "select * from wishlist where wishlist_id=?";
 	public static String GET_OWN_LIST = "select * from ownlist where ownlist_id=?";
-	
+	public static String ADD_OWN_LIST = "insert into ownlist values (?,?,?,?,?)";
+	public static String ADD_WISH_LIST = "insert into wishlist values (?,?,?,?,?)";
 
-	
 }
