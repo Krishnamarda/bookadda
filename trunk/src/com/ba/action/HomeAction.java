810dace21c2d31c417eea92bdca4ec0eb2a6875a
skjoshi13 <skjoshi13@2210af32-f2a1-504b-2c71-20ec49948f07>
2014-03-22 10:56:01 +0000

    Updated

--- trunk/src/com/ba/action/HomeAction.java
+++ trunk/src/com/ba/action/HomeAction.java
@@ -1,25 +1,40 @@
 package com.ba.action;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
+import org.omg.CORBA.Request;
+
 import com.ba.constants.Constants;
+import com.ba.dao.impl.HomeDAO;
+import com.ba.exception.BAException;
+import com.ba.factory.GenericFactory;
+import com.ba.pojo.Ownlist;
 import com.opensymphony.xwork2.ActionContext;
 
 public class HomeAction {
-
-	public String execute() throws Exception {
+	List<Ownlist> homeData =null;
+	public List<Ownlist> getHomeData() {
+		return homeData;
+	}
+	public void setHomeData(List<Ownlist> homeData) {
+		this.homeData = homeData;
+	}
+	public String execute() {
+		
 		Map<String, Object> session = ActionContext.getContext().getSession();
 		String unm = (String)session.get("username");
 		if(unm==null){
 			return null;
 		}
-		/*try {
+		try {
 			HomeDAO dao = (HomeDAO) GenericFactory
 					.buildObject(Constants.HOME_DAO);
-			dao.getHomeDetails(username);
+			homeData=dao.getHomeDetails(unm);
 		} catch (BAException e) {
-			System.out.println(e);
-		}*/
+		
+		}
 		return Constants.SUCCESS;
 	}
 }
