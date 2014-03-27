48b6067ef80a45468d2e46dee250fc25d91c41a8
skjoshi13 <skjoshi13@2210af32-f2a1-504b-2c71-20ec49948f07>
2014-03-27 16:43:53 +0000

    Updated

--- trunk/src/com/ba/dao/impl/HomeDAO.java
+++ trunk/src/com/ba/dao/impl/HomeDAO.java
@@ -4,7 +4,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,10 +25,8 @@ public class HomeDAO extends ConnectionFactory{
 			pStmt = con.prepareStatement(Constants.GET_HOME_DATA);
 			pStmt.setString(1, unm);
 			pStmt.setString(2, unm);
-			int dataCount=0;
 			rSet = pStmt.executeQuery();
 			while (rSet.next()) {
-				dataCount++;
 				Ownlist ol = new Ownlist();
 				ol.setOwnlist_id(rSet.getString(colCount++));
 				ol.setBook_name(rSet.getString(colCount++));
@@ -39,31 +36,6 @@ public class HomeDAO extends ConnectionFactory{
 				homeData.add(ol);
 				colCount=1;
 			}
-			/*rSet.next();
-			Ownlist ol = new Ownlist();
-			ol.setOwnlist_id(rSet.getString(colCount++));
-			ol.setBook_name(rSet.getString(colCount++));
-			ol.setAuthor(rSet.getString(colCount++));
-			ol.setAvailtype(rSet.getString(colCount++));
-			ol.setQuantity(rSet.getInt(colCount++));
-			homeData.add(ol);
-			colCount=1;
-			Ownlist ol1 = new Ownlist();
-			ol1.setOwnlist_id(rSet.getString(colCount++));
-			ol1.setBook_name(rSet.getString(colCount++));
-			ol1.setAuthor(rSet.getString(colCount++));
-			ol1.setAvailtype(rSet.getString(colCount++));
-			ol1.setQuantity(rSet.getInt(colCount++));
-			homeData.add(ol1);
-			colCount=1;
-			Ownlist ol2 = new Ownlist();
-			ol2.setOwnlist_id(rSet.getString(colCount++));
-			ol2.setBook_name(rSet.getString(colCount++));
-			ol2.setAuthor(rSet.getString(colCount++));
-			ol2.setAvailtype(rSet.getString(colCount++));
-			ol2.setQuantity(rSet.getInt(colCount++));
-			homeData.add(ol2);*/
-			System.out.println("Records - "+dataCount);
 		} catch (SQLException sqle) {
 			throw new BAException(sqle,
 					"Exception caught in UserDetails.userLogin()");
