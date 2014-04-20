e8ad5475dfaa30d871249e0c49189a554e29f36a
krishnamail55 <krishnamail55@gmail.com>
2014-04-20 09:16:15 +0000

    Load profile data

--- src/com/ba/action/ProfileAction.java
+++ src/com/ba/action/ProfileAction.java
@@ -2,13 +2,18 @@ package com.ba.action;
 
 import java.util.Map;
 
+import com.ba.constants.Constants;
+import com.ba.dao.impl.HomeDAO;
+import com.ba.dao.impl.ProfileDAO;
+import com.ba.exception.BAException;
+import com.ba.factory.GenericFactory;
+import com.ba.pojo.UserDetails;
 import com.opensymphony.xwork2.ActionContext;
 
 
 public class ProfileAction {
 
-	private String fname;
-	private String lname;
+	private UserDetails userDetails;
 	
 	public String execute() {
 		Map<String, Object> session = ActionContext.getContext().getSession();
@@ -16,22 +21,25 @@ public class ProfileAction {
 		if(unm==null){
 			return null;
 		}
+		
+		try {
+			ProfileDAO dao = (ProfileDAO) GenericFactory
+					.buildObject(Constants.PROFILE_DAO);
+			userDetails = dao.getProfileeDetails(unm);
+		} catch (BAException e) {
+		
+		}
+		
 		return "success";
 	}
 
-	public String getFname() {
-		return fname;
+	public UserDetails getUserDetails() {
+		return userDetails;
 	}
 
-	public void setFname(String fname) {
-		this.fname = fname;
+	public void setUserDetails(UserDetails userDetails) {
+		this.userDetails = userDetails;
 	}
 
-	public String getLname() {
-		return lname;
-	}
-
-	public void setLname(String lname) {
-		this.lname = lname;
-	}
+	
 }
